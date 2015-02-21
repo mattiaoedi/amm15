3509860332262c0d821b3b37217f94372c11dec7
mattiaoedi <chissenefrega@me.com>
2015-02-21 21:49:13 +0100

    Update admin_registra.php

--- admin_registra.php
+++ admin_registra.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
-$_SESSION['id_edit'] = '';
-$_SESSION['role_edit'] = '';
-$_SESSION['nome_edit']= '';
-$_SESSION['cognome_edit'] = '';
-$_SESSION['via_edit'] = '';
-$_SESSION['civico_edit'] = '';
-$_SESSION['citta_edit'] = '';
-$_SESSION['provincia_edit'] = '';
-$_SESSION['cap_edit'] = '';
-$_SESSION['email_edit'] = '';
-$_SESSION['ricevimento_edit'] = '';
+@$_SESSION['id_edit'] = '';
+@$_SESSION['role_edit'] = '';
+@$_SESSION['nome_edit']= '';
+@$_SESSION['cognome_edit'] = '';
+@$_SESSION['via_edit'] = '';
+@$_SESSION['civico_edit'] = '';
+@$_SESSION['citta_edit'] = '';
+@$_SESSION['provincia_edit'] = '';
+@$_SESSION['cap_edit'] = '';
+@$_SESSION['email_edit'] = '';
+@$_SESSION['ricevimento_edit'] = '';
 // includiamo il file di connessione al database
 include ('files/config.php');
 
@@ -67,14 +67,14 @@ function set_corso() {
 <?php include 'include/lside.htm'; ?>
 <? 
 //controllo docente
-if ($_SESSION['login'] == "Yes" && $_SESSION['role'] == 'admin') {
+if (@$_SESSION['login'] == "Yes" && @$_SESSION['role'] == 'admin') {
 ?>
   <page class="content">
     <section>
      <h2 class="icona" id="libretto-m">Registra esame</h2>
       <p>&nbsp;</p>
-      <p><strong>Nome: </strong> <? echo $_SESSION['nome'] ?></p>
-      <p><strong>Cognome: </strong> <? echo $_SESSION['cognome'] ?></p>
+      <p><strong>Nome: </strong> <? echo @$_SESSION['nome'] ?></p>
+      <p><strong>Cognome: </strong> <? echo @$_SESSION['cognome'] ?></p>
       <p><strong>Gestione come amministratore</strong></p>
       <hr width="100%" size="2" color="1c345a">
         <h3>Registra voto esame</h3>
@@ -115,9 +115,9 @@ if ($_SESSION['login'] == "Yes" && $_SESSION['role'] == 'admin') {
 <?php
 // attraverso un if controlliamo che il form sia stato inviato
 
-if ( $_GET['add'] == "voto" ) {
+if ( @$_GET['add'] == "voto" ) {
 
-$id_docente = $_SESSION['id'];
+$id_docente = @$_SESSION['id'];
 // recuperiamo i dati inviati con il form
 $insegnamento = $_POST['insegnamento'];
 $matricola = $_POST['matricola'];
@@ -162,7 +162,7 @@ echo "<img src='files/img/no.png' width='32' height='32' alt='no' style='vertica
     </ul>
   </rside>
 <?
-} elseif ($_SESSION['login'] != "Yes") {
+} elseif (@$_SESSION['login'] != "Yes") {
 
 	
 echo "<page class='content'><section><center><img src='files/img/no.png' width='32' height='32' alt='accesso negato'style='vertical-align:middle;' /><b>Accesso non autorizzato.</b><p>&nbsp;</p><a href='index.php?page=login'><input id='button' type='submit' alt='login' value='login'/></a><p>&nbsp;</p><a href='index.php?page=registrazione'><input id='button' type='submit' alt='registrati' value='registrati'/></a></center></section></page>
