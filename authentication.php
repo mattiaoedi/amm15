04a63a591f0d5d7e573a239f8277a9e264484466
mattiaoedi <chissenefrega@me.com>
2015-02-22 11:51:12 +0100

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -17,7 +17,7 @@ include ('files/config.php');
 if ( (isset($_GET['check']) && ($_GET['check'] == "logout"))) {
 	header( "refresh:1;url=index.php" );
 	}	
-	if (isset($_SESSION['role']) && ($_SESSION['role'] == 'studente')) {
+	if ((isset($_GET['check']) && ($_GET['check'] == "logout")) && (isset($_SESSION['role']) && ($_SESSION['role'] == 'studente'))) {
 	header("Location: /studente_home.php");
 	}	
 ?>
