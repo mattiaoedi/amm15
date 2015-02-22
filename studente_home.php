8928e5f362a4368df70131138990636d30240252
mattiaoedi <chissenefrega@me.com>
2015-02-22 14:28:12 +0100

    Update studente_home.php

--- studente_home.php
+++ studente_home.php
@@ -1,84 +1,63 @@
+<lside> 
+	<h2>Benvenuto!</h2>
+      <p><a href="index.php">Descrizione</a></p>
 <?php
-session_start();
-@$_SESSION['id_edit'] = '';
-@$_SESSION['role_edit'] = '';
-@$_SESSION['nome_edit']= '';
-@$_SESSION['cognome_edit'] = '';
-@$_SESSION['via_edit'] = '';
-@$_SESSION['civico_edit'] = '';
-@$_SESSION['citta_edit'] = '';
-@$_SESSION['provincia_edit'] = '';
-@$_SESSION['cap_edit'] = '';
-@$_SESSION['email_edit'] = '';
-@$_SESSION['ricevimento_edit'] = '';
-// includiamo il file di connessione al database
-include ('files/config.php');
 
-    $url = basename($_SERVER['PHP_SELF']);
-?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>amm15 - Università di Cagliari</title>
-<link href="files/css.css" rel="stylesheet" type="text/css" />
-</head>
+$url = basename($_SERVER['PHP_SELF']);
 
-<body>
-<div class="container">
-<div class="header">
-	<font size="5" color="#FFFFFF"><b><p>Università degli Studi di Cagliari<br>
-	</p>
-	<p>amm15</p></b></font>
-</div>
-<?php include 'include/lside.htm'; ?>
-<? 
-//controllo studente
-if (($_SESSION['login'] == "yes" && $_SESSION['role'] == "studente")) {
-?>
-<page class="content">
-    <section>
-      <h2 class="icona" id="areap">Area personale</h2>
-      <p>Benvenuto, <? echo @$_SESSION['nome'] ?>.</p>
-      <p>Benvenuto nella tua area riservata!</p>
-	  <div class="box_link">
-      <p><center><li><a href="studente_anagrafica.php" id="anagrafica">Anagrafica</a></li>
-                    <li><a href="studente_libretto.php" id="libretto">Libretto</a></li>
-                    <li><a href="studente_iscrivi.php" id="esame">Iscrizione</a></li></center></p>
-					</div>
-    </section>
-  <!-- end .content --></page>  
-  <rside>
-    <h2>Informazioni</h2>
-    <p>Seleziona una delle  seguenti funzionalità disponibili per la gestione dei tuoi esami:</p>
-      <li><strong>Anagrafica</strong> per modificare i tuoi dati anagrafici e la tua password.</li>
-      <li><strong>Libretto</strong> per visualizzare gli esami già sostenuti.</li>
-      <li><strong>Iscrizione</strong> per iscriversi ad un appello di esame.</li>
-  </rside>
-  <?
-} elseif (@$_SESSION['login'] != "Yes") {
-
-	
-echo "<page class='content'><section><center><img src='files/img/no.png' width='32' height='32' alt='accesso negato'style='vertical-align:middle;' /><b>Accesso non autorizzato.</b><p>&nbsp;</p><a href='index.php?page=login'><input id='button' type='submit' alt='login' value='login'/></a><p>&nbsp;</p><a href='index.php?page=registrazione'><input id='button' type='submit' alt='registrati' value='registrati'/></a></center></section></page>
-	<rside>
-    <h2>Informazioni</h2>
-    <p>Per poter visualizzare questa pagina devi essere uno studente:</p>
-      <li><strong>Login</strong>, esegui il login come studente.</li>
-      <li><strong>Registrati</strong>, per diventare uno studente.</li>
-  </rside>";
-
-} elseif ( isset($_GET['check']) && $_GET['check'] == "logout" ) {} else {
-	
-echo "<page class='content'><section><center><img src='files/img/no.png' width='32' height='32' alt='accesso negato' style='vertical-align:middle;' /><b>Accesso non autorizzato.</b></center></section></page>
-	<rside>
-    <h2>Informazioni</h2>
-    <p>Per poter visualizzare questa pagina devi essere uno studente:</p>
-      <li><strong>Login</strong>, esegui il login come studente.</li>
-  </rside>";	
-
+if ((((isset($_SESSION['login']) && $_SESSION['login']) =="yes" )) && (isset($_SESSION['role']) && ($_SESSION['role'] == "studente" && $_SESSION['role'] != "docente" && $_SESSION['role'] != "admin"))) {?>
+    <h2>Area riservata</h2>
+      <p>Ciao, <? echo $_SESSION['nome'] ?>  <a href="<? echo $url ?>?check=logout"><img src="files/img/logout.png" width="16" height="16" alt="logout" style="vertical-align:middle;"></a></p>
+      <p><a href="studente_home.php">Home</a></p>
+      <p><a href="studente_anagrafica.php">Anagrafica</a></p>
+      <p><a href="studente_libretto.php">Libretto</a></p>
+      <p><a href="studente_iscrivi.php">Esami</a></p>
+    <p><!-- end .sidebar1 --></p>
+<?php
+} elseif ((((isset($_SESSION['login']) && $_SESSION['login']) =="yes" )) && (isset($_SESSION['role']) && ($_SESSION['role'] != "studente" && $_SESSION['role'] == "docente" && $_SESSION['role'] != "admin"))) {?>
+    <h2>Area riservata</h2>
+      <p>Ciao, <? echo $_SESSION['nome'] ?>  <a href="<? echo $url ?>?check=logout"><img src="files/img/logout.png" width="16" height="16" alt="logout" style="vertical-align:middle;"></a></p>
+      <p><a href="docente_home.php">Home</a></p>
+      <p><a href="docente_anagrafica.php">Anagrafica</a></p>
+      <p><a href="docente_appelli.php">Appelli</a></p>
+      <p><a href="docente_registra.php">Registra esame</a></p>
+      <p><a href="docente_elenco.php">Elenco esami</a></p>
+<?php
+} elseif ((((isset($_SESSION['login']) && $_SESSION['login']) =="yes" )) && isset($_SESSION['role']) && ($_SESSION['role'] != "studente" && $_SESSION['role'] != "docente" && $_SESSION['role'] == "admin")) { ?>
+    <h2>Area riservata</h2>
+      <p>Ciao, <? echo $_SESSION['nome'] ?>  <a href="<? echo $url ?>?check=logout"><img src="files/img/logout.png" width="16" height="16" alt="logout" style="vertical-align:middle;"></a></p>
+      <p><a href="admin_home.php">Home</a></p>
+      <p><a href="admin_studenti.php">Studenti</a></p>
+      <p><a href="admin_docenti.php">Docenti</a></p>
+      <p><a href="admin_appelli.php">Appelli</a></p>
+      <p><a href="admin_registra.php">Registra esami</a></p>
+      <p><a href="admin_elenco.php">Elenco esami</a></p>
+      <p><a href="admin_quadro.php">Dip &amp; CdL</a></p>
+<?php
+} else {
+?> 
+    <h2>Area riservata</h2>
+      <p><a href="index.php?page=login">Login</a></p>
+      <p><a href="index.php?page=registrazione">Registrazione</a></p>
+<?php
 }
 ?>
-<?php include 'include/footer.htm'; ?>
-  <!-- end .container --></div>
-</body>
-</html>
+    <p><!-- end .sidebar1 --></p>
+        <h2>Didattica</h2>
+      <p><a href="index.php?page=offerta">Offerta formativa</a></p>
+      <p><a href="http://www.unica.it/" target="_blank">Università di Cagliari</a></p>
+    <p><!-- end .sidebar1 --></p>
+</lside>
+<?php
+/*logout*/
+// attraverso un if controlliamo che il form sia stato inviato
+	if ( isset($_GET['check']) && $_GET['check'] == "logout" ) {
+//Distruggo la vecchia sessione		
+	$_SESSION['login'] = "no";
+	session_unset();
+	session_destroy();
+//Apro una nuova sessione	
+	session_start();
+	echo "<img src='files/img/ok.png' width='32' height='32' alt='ok' style='vertical-align:middle;' /><b>Logout effettuato con successo.</b><p>&nbsp;</p><a href='index.php'><input id='button' type='submit' alt='home page' value='home page'/></a><p>&nbsp;</p><a href='index.php?page=login'><input id='button' type='submit' alt='login' value='login'/></a>";
+}
+?>
